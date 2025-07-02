599164f33e3318841eca819ecd625c5c47396e5f
MarianSantos14 <mariana.carvalho1409@outlook.com>
2025-07-02 14:10:22 -0300

    Juntando e estilizando as paginas

--- js/metodoForEach.js
+++ js/metodoForEach.js
@@ -9,17 +9,17 @@ function exibirOsLivrosNaTela(listaDeLivros) {
         let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livros__imagens indisponivel'
 
         elementoParaInserirLivros.innerHTML += `
-            <div class="livro">
-            <img class="${disponibilidade}" src="${livro.imagem}"
-                alt="${livro.alt}" />
-            <h2 class="livro__titulo">
-                ${livro.titulo}
-            </h2>
-            <p class="livro__descricao">${livro.autor}</p>
-            <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
-            <div class="tags">
-                <span class="tag">${livro.categoria}</span>
-            </div>
+            <div class="livro anim-livro">
+                <div class="tags">
+                    <span class="tag">${livro.categoria}</span>
+                </div>
+                <img class="${disponibilidade}" src="${livro.imagem}"
+                    alt="${livro.alt}" />
+                <h2 class="livro__titulo">
+                    ${livro.titulo}
+                </h2>
+                <p class="livro__descricao">${livro.autor}</p>
+                <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
             </div>
         `
     });
